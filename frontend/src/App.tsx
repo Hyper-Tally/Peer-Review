db4eab26d5b22f8c6fe47a3bc1903af5718c8ea7
Mintah <130777897+Hyper-Tally@users.noreply.github.com>
2024-03-07 18:09:33 +0300

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Navbar from './components/Navbar';
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/Home';
+import UploadPaperScreen from './pages/UploadPaperScreen/UploadPaperScreen';
+import SignUpLoginInScreen from './pages/SignUpLoginInScreen/SignUpLoginInScreen';
+import LoadingScreen from './pages/LoadingScreen/LoadingScreen';
+import { UserContextProvider } from './contexts/user';
+import ProfileScreen from './pages/ProfileScreen/ProfileScreen';
+import ReviewPaper from './pages/ReviewPaper/ReviewPaper';
+import PageNotFoundScreen from './pages/PageNotFoundScreen/PageNotFoundScreen';
+import BrowseScreen from './pages/BrowseScreen/BrowseScreen';
+
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <UserContextProvider>
+          <Navbar />
+          <Routes>
+            <>
+              <Route path="/ok" element={<Home />} />
+              <Route path="/paper" element={<UploadPaperScreen />} />
+              <Route path="/" element={<SignUpLoginInScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
+              <Route path="/view/:address" element={<ReviewPaper />} />
+              <Route path="/browse" element={<BrowseScreen />} />
+              <Route path="*" element={<PageNotFoundScreen />} />
+            </>
+          </Routes>
+        </UserContextProvider>
+      </Router>
+
+    </>
+  );
+}
+
+export default App;
